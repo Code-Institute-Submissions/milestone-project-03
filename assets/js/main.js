d06d0f3e4239c5966ea41b27769599b6331ccbbb
trisdauvergne <tristan.dauvergne@gmail.com>
2020-03-03 11:28:09 +0000

    200303 12:27 Added and tested first search from Deezer website

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+var data = null;
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+// Practise search function
+function search() {
+
+    let inputValue = document.getElementById("search").value;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("GET", "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + inputValue);
+    xhr.setRequestHeader("x-rapidapi-host", "deezerdevs-deezer.p.rapidapi.com");
+    xhr.setRequestHeader("x-rapidapi-key", "188d30da21msh99fa3832c206cd5p1eb131jsn0acc1b025fc9");
+
+    xhr.send(data);
+
+}
