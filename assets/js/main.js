f1641478afc5353bd7468d81713d862c17d0a15b
trisdauvergne <tristan.dauvergne@gmail.com>
2020-03-06 16:30:45 +0000

    200306 - 17:30 code for artist search with filter, duplicate functionality and buttons being saved before trying to make buttons live

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ var data = null;
 var xhr = new XMLHttpRequest();
 xhr.withCredentials = true;
 
-// Initial search function
+// First user step: artist name search function
 function search() {
 
     let inputValue = document.getElementById("search").value;
@@ -24,58 +24,43 @@ function search() {
                 if (initialArtistNames.toLowerCase().includes(inputValue.toLowerCase())) {
                     if (artistList.includes(initialArtistNames) == false) {
                         artistList.push(initialArtistNames);
-                        document.getElementById("artist-name").innerHTML += "<p>" + initialArtistNames + "</p>";
-                        // document.getElementById("artist-name").innerHTML += initialArtistNames + "<br>";
+                        // var btn = document.createElement("BUTTON"); // new line
+                        // btn.innerHTML += initialArtistNames; // new line
+                        // document.getElementById("artist-name").appendChild(btn); // new line
+                        document.getElementById("artist-name").innerHTML += "<button>" + initialArtistNames + "</button>" + "<br>";
+                        console.log(initialArtistNames);
                     }
                 }
             }
-
-            /*  
-                          if (filteredArtistNames.includes(inputValue.toLowerCase())) {
-                                // BELOW 4 LINES ARE EARLY STEPS OF TRYING TO FIGURE OUT FILTER METHOD. CAN BE REMOVED IF NOT POSSIBLE.
-                                var finalArtistNames = filteredArtistNames.filter(function (name, index) {
-                                    function removeDuplicates(data) {
-                                        return data.filter((value, index) = data.indexOf(value) === filteredArtistNames.indexOf(name) === index);
-                                    }
-                                    console.log(removeDuplicates(finalArtistNames));
-                                });
-                                document.getElementById("artist-name").innerHTML += responseData[i].artist.name + "<br>";
-                            } else {
-                                console.log("this, has, not, worked.")
-                            }*/
-            // document.getElementById("artist-name").innerHTML += responseData[i].artist.name + "<br>";
-            // document.getElementById("popular-song").innerHTML += "Popular song: " + responseData[i].title + "<br>";
-            // document.getElementById("preview-song").innerHTML += "Song preview: " + responseData[i].preview + "<br>";                
-            // document.getElementById("recommended-album").innerHTML += "Recommended album: " + responseData[i].album.title + "<br>";
-            // to display each name: create an empty string. then use a for loop, and for each name in artistList, append the following to the string:
-            // string += "<p>" + artistList[i] + "</p>";
-            // innerHtml of artist div = string
-
         }
     });
 
     xhr.open("GET", "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + inputValue);
     xhr.setRequestHeader("x-rapidapi-host", "deezerdevs-deezer.p.rapidapi.com");
     xhr.setRequestHeader("x-rapidapi-key", "188d30da21msh99fa3832c206cd5p1eb131jsn0acc1b025fc9");
-
     xhr.send(data);
 
 }
 
+// // Second function: When the user clicks on an artist name, that element generates a new search for that artist's popular songs
+// function artistSongs(artistName) {
+    
+//     xhr.addEventListener("readystatechange", function () {
+//         if (this.readyState === this.DONE) {
+//             console.log(this.responseText);
+//             var response = JSON.parse(this.responseText);
+// }
+
+            // CODE FOR DISPLAYING DIFFERENT ELEMENTS
+            // document.getElementById("artist-name").innerHTML += responseData[i].artist.name + "<br>";
+            // document.getElementById("popular-song").innerHTML += "Popular song: " + responseData[i].title + "<br>";
+            // document.getElementById("preview-song").innerHTML += "Song preview: " + responseData[i].preview + "<br>";                
+            // document.getElementById("recommended-album").innerHTML += "Recommended album: " + responseData[i].album.title + "<br>";
+        
 
         // QUESTIONS
-        // Q1 How to make search specific eg. artist name, rather than songs which also have the artist name in it?
-        // - I have tried if (xxx.includes(inputValue === true, so whatever the name input in the search is)) {
-            // console.log(success!);
-            // } else {
-            // console.log(failure!);
-            // }
-            // ... but only shows failure
-            // - The 'in' operator
-        // Q2. How to show results once, rather than duplicates eg. names
         // Q3. How to make it so that when an artist name pops up, you can click a name and then it shows the song results and suggested albums?
         // Q4. How to make previews show up as images / preview bars
-        // Q5. Do I need to do anything with the other API GET Keys eg. artist / album?
 
 
 
