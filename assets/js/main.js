fd7170c201e33995008d11fa469a19b74b6491bb
trisdauvergne <tristan.dauvergne@gmail.com>
2020-03-07 15:04:12 +0000

    200307 - 16:03 linked function to buttons. Function currently works incorrectly

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,7 @@ function search() {
                         // var btn = document.createElement("BUTTON"); // new line
                         // btn.innerHTML += initialArtistNames; // new line
                         // document.getElementById("artist-name").appendChild(btn); // new line
-                        document.getElementById("artist-name").innerHTML += "<button>" + initialArtistNames + "</button>" + "<br>";
+                        document.getElementById("artist-name").innerHTML += `<button id="artists" onclick="artistSong('${initialArtistNames}')">` + initialArtistNames + "</button>" + "<br>";
                         console.log(initialArtistNames);
                     }
                 }
@@ -43,24 +43,42 @@ function search() {
 }
 
 // // Second function: When the user clicks on an artist name, that element generates a new search for that artist's popular songs
-// function artistSongs(artistName) {
-    
+function artistSong(artistName) {
+    console.log("function is still linked");
+    let songSearch = document.getElementById("search").value; // equivalent to the inputValue, USED "SEARCH" for now to test it works
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            console.log("the second part works");
+            var response = JSON.parse(this.responseText);
+
+            // The data received from the search function
+            var responseData = response.data;
+        }
+    });
+
+    xhr.open("GET", "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + songSearch);
+    xhr.setRequestHeader("x-rapidapi-host", "deezerdevs-deezer.p.rapidapi.com");
+    xhr.setRequestHeader("x-rapidapi-key", "188d30da21msh99fa3832c206cd5p1eb131jsn0acc1b025fc9");
+    xhr.send(data);
+}
+
+
+
+
+
 //     xhr.addEventListener("readystatechange", function () {
 //         if (this.readyState === this.DONE) {
 //             console.log(this.responseText);
 //             var response = JSON.parse(this.responseText);
-// }
+// 
 
             // CODE FOR DISPLAYING DIFFERENT ELEMENTS
             // document.getElementById("artist-name").innerHTML += responseData[i].artist.name + "<br>";
             // document.getElementById("popular-song").innerHTML += "Popular song: " + responseData[i].title + "<br>";
             // document.getElementById("preview-song").innerHTML += "Song preview: " + responseData[i].preview + "<br>";                
             // document.getElementById("recommended-album").innerHTML += "Recommended album: " + responseData[i].album.title + "<br>";
-        
+
 
         // QUESTIONS
         // Q3. How to make it so that when an artist name pops up, you can click a name and then it shows the song results and suggested albums?
         // Q4. How to make previews show up as images / preview bars
-
-
-
